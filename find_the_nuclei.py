d9f81644c535987c478a21e68b66a4224b5b64c8
lmataa <l.mataa@alumnos.upm.es>
2019-04-12 21:28:47 +0200

    imports done right, some params initializated

---
+++ find_the_nuclei.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import random
+import warnings
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
+from itertools import chain
+from skimage.io import imread, imshow, imread_collection, concatenate_images
+from skimage.transform import resize
+from skimage.morphology import label
+
+from keras.models import Model, load_model
+from keras.layers import Input
+from keras.layers.core import Dropout, Lambda
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.merge import concatenate
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras import backend as K
+
+import tensorflow as tf
+
+# Image parameters
+# Images are 128x128x3
+IMG_WIDTH = 128
+IMG_HEIGHT = 128
+IMG_CHANNELS = 3
+# Train and test data
+TRAIN_PATH = './input/train/'
+TEST_PATH = './input/test/'
